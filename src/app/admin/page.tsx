261d2e59c731028f5c1b4dde15ec80b7812d4735
Sharad Banga <sharadiit03@gmail.com>
2025-04-25 20:53:50 +0530

    adding collections

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,9 @@
 export default function admin(){
   return(
-    <>
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="bg-red-100 text-black w-20 p-4 ">
     ADMIN
-    </>
+    </div>
+    </div>
   )
 }
